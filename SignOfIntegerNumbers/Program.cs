534d44d39628edd82d303707c5916e831c3afb62
Remzi <remzitdf@abv.bg>
2021-02-08 18:03:27 +0200

    Add files via upload

---
+++ SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string result = Sign(num);
+            Console.WriteLine(result);
+        }
+        static string Sign(int number) {
+            string sign = null;
+            if (number>0)
+            {
+                sign = "positive";
+            }
+            else if (number<0)
+            {
+                sign = "negative";
+            }
+            else
+            {
+                sign = "zero";
+            }
+            return $"The number {number} is {sign}.";
+        }
+    }
+}
