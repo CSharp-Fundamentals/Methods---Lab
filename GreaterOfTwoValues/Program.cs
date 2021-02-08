da0788994b228382a40b41a20add320df4f99ad6
Remzi <remzitdf@abv.bg>
2021-02-08 17:58:23 +0200

    Add files via upload

---
+++ GreaterOfTwoValues/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace GreaterOfTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            if (type=="int")
+            {
+                int firstNum = int.Parse(Console.ReadLine());
+                int secondNum = int.Parse(Console.ReadLine());
+                Console.WriteLine(MaxInt(firstNum, secondNum));
+            }
+            else if (type=="char")
+            {
+                char ch1 = char.Parse(Console.ReadLine());
+                char ch2 = char.Parse(Console.ReadLine());
+                Console.WriteLine(MaxChar(ch1,ch2));
+            }
+            else if (type=="string")
+            {
+                string first = Console.ReadLine();
+                string second = Console.ReadLine();
+                Console.WriteLine(MaxString(first,second));
+            }
+        }
+        static int MaxInt(int firstNum, int secondNum) {
+            if (firstNum>secondNum)
+            {
+                return firstNum;
+            }
+            else
+            {
+                return secondNum;
+            }
+        }
+        static char MaxChar(char ch1, char ch2) {
+            if (ch1>ch2)
+            {
+                return ch1;
+            }
+            else
+            {
+                return ch2;
+            }
+        }
+        static string MaxString(string first, string second) {
+            if (first.CompareTo(second) >= 0)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
---
+++ MathOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            int secondNum = int.Parse(Console.ReadLine());
+            Operation(firstNum,operation,secondNum);
+        }
+        static void Operation(int firstNum, string operation, int secondNum) {
+            int sum = 0;
+            if (operation=="+")
+            {
+                sum =firstNum + secondNum;
+            }
+            else if (operation=="-")
+            {
+                sum =firstNum - secondNum;
+            }
+            else if (operation == "/")
+            {
+                sum =firstNum / secondNum;
+            }
+            else if (operation == "*")
+            {
+                sum =firstNum * secondNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
